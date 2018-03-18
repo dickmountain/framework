f4d8e366ac60e52fa4dde7f037d1ddfcedec5fac
maxatroshenko <maxatrr@gmail.com>
2018-03-18 18:35:59 +0300

    Views with twig

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -2,15 +2,21 @@
 
 namespace App\Controllers;
 
+use App\Views\View;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
 
 class HomeController
 {
+	protected $view;
+
+	public function __construct(View $view)
+	{
+		$this->view = $view;
+	}
+
 	public function index(RequestInterface $request, ResponseInterface $response)
 	{
-		$response->getBody()->write(123);
-
-		return $response;
+		return $this->view->render($response, 'home.twig', []);
 	}
 }
--- bootstrap/container.php
+++ bootstrap/container.php
@@ -2,4 +2,9 @@
 
 $container = new League\Container\Container;
 
+$container->delegate(
+	new \League\Container\ReflectionContainer
+);
+
 $container->addServiceProvider(new App\Providers\AppServiceProvider());
+$container->addServiceProvider(new App\Providers\ViewServiceProvider());
