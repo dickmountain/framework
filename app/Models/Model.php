9d568eda85b0b96d59b7af855fcd475c24c734dd
maxatroshenko <maxatrr@gmail.com>
2018-03-29 22:31:55 +0300

    Model Magic Methods

---
+++ app/Models/Model.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+
+abstract class Model
+{
+	public function __get($name)
+	{
+		if (property_exists($this, $name)) {
+			return $this->name;
+		}
+
+		return null;
+	}
+
+	public function __isset($name)
+	{
+		if (property_exists($this, $name)) {
+			return true;
+		}
+
+		return false;
+	}
+}
